e03d997fd77948eb11f7fb98da931ba3d2e98b3f
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2025-09-29 16:13:55 +0700

    revert wrong commit

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,18 +2,18 @@ import { Header } from "./app/components/layout/Header";
 import { Toolbar } from "./app/components/layout/Toolbar";
 import { LeftPanel } from "./app/components/layout/LeftPanel";
 import { MainView } from "./app/components/layout/MainView";
-import { TestJsonAutoForm } from "./dev/TestJsonAutoForm";
+// import { TestJsonAutoForm } from "./dev/TestJsonAutoForm";
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
-      {/* <Header />
+      <Header />
       <Toolbar />
       <div className="flex flex-1 overflow-hidden">
         <LeftPanel />
         <MainView />
-      </div> */}
-      <TestJsonAutoForm />
+      </div>
+      {/* <TestJsonAutoForm /> */}
     </div>
   );
 }
